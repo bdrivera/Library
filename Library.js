4690297d7455d45ceb7648fa48826524e664f0b2
Bret Rivera <bdrrivera@gmail.com>
2021-01-28 14:08:12 -0800

    Initial Commit -Added framework css, html, and Javascript files

---
+++ Library.js
@@ -0,0 +1,17 @@
+'use strict'
+function Book(title, author, pagesCount, read) {
+    this.title = title;
+    this.author = author;
+    this.pagesCount = pagesCount;
+    this.readStatus = read;
+
+    let myLibrary = [];
+
+    function Book() {
+
+    }
+    
+    function addBookToLibrary() {
+
+    }
+}
