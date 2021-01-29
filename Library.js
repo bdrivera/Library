23d63e8c9adad8d94bf182e34bc3518ca9b13c0c
Bret Rivera <bdrrivera@gmail.com>
2021-01-29 15:40:57 -0800

    Changed appending webpage, and added remove button -Simplified appending and clearing of objects to webpage -Added remove button to individual books

--- Library.js
+++ Library.js
@@ -1,9 +1,6 @@
 'use strict'
 
 const libraryContainer = $('#libraryContainer');
-const bookHeight = 200;
-const bookWidth = 200;
-
 let myLibrary = [];
 let locationCounter = 0;
 
@@ -22,8 +19,6 @@ function Book(title, author, pageCount, readStatus) {
     this.pageCount = pageCount;
     this.readStatus = readStatus;
     this.element = document.createElement('div');
-    this.displayValue = [["", title], ["by ", author],
-                            [pageCount, "pgs"], ["", readStatus]];
     this.location;
 }
 
@@ -39,19 +34,43 @@ function addBookToLibrary(title, author, pageCount, readStatus) {
     newBook.location = myLibrary.length; //set newBooks location for reference later
     myLibrary.push(newBook); //add newBook to the myLibrary array
 
-    newBook.element.className = "book";
-
-    for(let i of newBook.displayValue) {
-        console.log(i[0] + " + " + i[1]);
-        newBook.element.innerHTML += i[0] + i[1] + "<br/>";
-    }
-
-    libraryContainer.appendChild(newBook.element);
+    assembleBook(newBook);
+    refreshLibrary();
 }
 
+function assembleBook(book) {
+    book.element.className = "book";
 
-function removeBookFromLibrary() {
+    let left = document.createElement('div');
+    left.className = 'book-left';
+    left.innerHTML = book.title + "<br/>" + "by " + book.author;
 
+    let right = document.createElement('div');
+    right.className = 'book-right';
+    right.innerHTML = "<div class='book-right'>" + book.pageCount + "pg <br/></div>"
+
+    const remButton = document.createElement('button');
+    //remButton.id = 'removeBook';
+    remButton.innerHTML = 'Remove';
+    remButton.className = 'book-right';
+
+    book.element.appendChild(left);
+    right.appendChild(remButton);
+    book.element.appendChild(right);
+}
+
+function removeBookFromLibrary(book) {
+
+}
+
+function refreshLibrary() {
+    /*for(let i of myLibrary) {
+        libraryContainer.removeChild(i);
+    }*/
+
+    for(let i of myLibrary) {
+        libraryContainer.appendChild(i.element);
+    }
 }
 
 /**
@@ -72,28 +91,28 @@ function addGeneralButtonListeners() {
 }
 
 /**
- * Returns title of new book from user input.
+ * Returns title from user input.
  */
 function getInputTitle() {
     return $('#newTitle').value;
 }
 
 /**
- * Returns author of new book from user input.
+ * Returns author from user input.
  */
 function getInputAuthor() {
     return $('#newAuthor').value;
 }
 
 /**
- * Returns page count of new book from user input.
+ * Returns page count from user input.
  */
 function getInputPageCount() {
     return parseInt($('#newPages').value);
 }
 
 /**
- * Returns read status of new book from user input.
+ * Returns read status from user input.
  */
 function getInputReadStatus() {
     return $('#newRead').value;
